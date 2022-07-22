d010e7bfa36737dc648e783a12437306e781d2a3
Timothy Scheuering <timsch@cisco.com>
2022-07-22 16:50:41 -0400

    feat(package): set up initial build tooling

---
+++ packages/components/provider/esbuild.config.js
@@ -0,0 +1,3 @@
+import { component } from '../../../config/esbuild/esbuild.config.js';
+
+component();
--- packages/components/provider/src/Provider.ts
+++ packages/components/provider/src/Provider.ts
@@ -1,5 +1,22 @@
-import { LitElement } from 'lit';
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 
-export default class Provider extends LitElement {
+@customElement('md-provider')
+class Provider extends LitElement {
+  static override styles = css`p { color: blue }`;
 
+  @property()
+  name = 'Somebody';
+
+  public override render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+
+  public static get CONSTANTS() {
+    return {
+      value: 'value'
+    };
+  }
 }
+
+export default Provider;
